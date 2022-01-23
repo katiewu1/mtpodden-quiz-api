c372d8ae06bd6fdac29afa17bd58d1dd2c9037b6
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2022-01-23 22:17:48 +0100

    initial commit, add connection to MongoDB and install mongoose. Create a schema och model.

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/mtpoddenQuiz'
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+mongoose.Promise = Promise
+
+// Schema and Model for the DB
+const SubmissionSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  profession: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  answers: {
+    type: Array,
+    required: true,
+  },
+})
+
+const Submission = mongoose.model('Submission', SubmissionSchema)
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -13,10 +49,22 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// Unreachable database -> status 503
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
 })
+
+// get all submissions
+// post a submission
 
 // Start the server
 app.listen(port, () => {
